80492f3914c484be8f98f52f7f6003f1182c1edd
nutkagorskaya <anutagor@yandex.ru>
2025-05-06 09:15:41 +0300

    commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,7 +15,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: 'git@github.com:aErema/nodejs-mesto-project.git',
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'pre-deploy-local': `pwd && scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': 'pwd && cd /nodejs-mesto-project && pwd && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
